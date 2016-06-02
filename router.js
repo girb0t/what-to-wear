3fef0156c1b7710f19741e1d736d668d3877b40b
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-02 19:11:14 -0400

    create index.html, index.js

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.send({ hello: 'world' });
+    // res.send({ hello: 'world' });
+    res.sendFile(__dirname + "/app/index.html");
   });
 };
