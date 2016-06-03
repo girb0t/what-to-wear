fe4cdc4ef91b41823df9303034c09d5bfe82df92
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-02 23:50:49 -0400

    Successfully render React component via react router

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+// import promise from 'redux-promise';
+
+import routes from './routes';
+// import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware(
+//   promise
+// )(createStore);
+
+const store = createStore(state => state); // PLACEHOLDER
+
+ReactDOM.render(
+  // <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.querySelector('.container')
+);
