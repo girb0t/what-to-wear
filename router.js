e1857075697f839cfe596f4997ffe3e6df51b0d4
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-03 20:48:57 -0400

    fix routing bug

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
 module.exports = function(app, express) {
-  app.get('/', function(req, res) {
+  //allow static files (CSS/JS files) to be served
+  app.use(express.static('app'));
+
+  app.get('/*', function(req, res) {
     // res.send({ hello: 'world' });
     res.sendFile(__dirname + "/app/index.html");
   });
-
-  //allow static files to be served
-  app.use(express.static('app'));
 };
