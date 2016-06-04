fcb688efdb7c865cd830d056d9e89d20dfc17da9
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-04 18:59:50 -0400

    Create stubbed prototype of autosuggest location searchbar

--- app/containers/location_form.js
+++ app/containers/location_form.js
@@ -1,9 +1,82 @@
 import React, { Component } from 'react';
+import Autosuggest from 'react-autosuggest';
 
 class LocationForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: this.getSuggestions('')
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionsUpdateRequested = this.onSuggestionsUpdateRequested.bind(this);
+  }
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'New York',
+      value,
+      onChange: this.onChange
+    };
+
     return (
-      <h2>Location Form</h2>
+      <div>
+        <h2>Location Form</h2>
+        <Autosuggest suggestions={suggestions}
+                     onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+                     getSuggestionValue={this.getSuggestionValue}
+                     renderSuggestion={this.renderSuggestion}
+                     inputProps={inputProps} />
+      </div>
+    );
+  }
+
+  onChange(event, { newValue }) {
+    this.setState({
+      value: newValue
+    });
+  }
+
+  onSuggestionsUpdateRequested({ value }) {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  }
+
+  getLocations(term) {
+    return [
+      {
+        description: 'New York, NY, USA'
+      },
+      {
+        description: 'Boston, MA, USA'
+      },
+      {
+        description: 'Oakland, CA, USA'
+      },
+      {
+        description: 'Kiev, Ukraine'
+      }
+    ]
+  }
+
+  getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : this.getLocations(inputValue);
+  }
+
+  getSuggestionValue(suggestion) { // when suggestion selected, this function tells what should be the value of the input
+    return suggestion.description;
+  }
+
+  renderSuggestion(suggestion) {
+    return (
+      <span>{suggestion.description}</span>
     );
   }
 }
