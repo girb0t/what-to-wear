2c7642ceb9135442269293a5171d4e7778f90562
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-05 10:31:00 -0400

    Make LocationSearchBar its own component

---
+++ app/components/location_search_bar.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import Autosuggest from 'react-autosuggest';
+
+class LocationSearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: this.getSuggestions('')
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
+    this.onSuggestionsUpdateRequested = this.onSuggestionsUpdateRequested.bind(this);
+  }
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'New York',
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <div>
+        <h2>Location Form</h2>
+        <Autosuggest suggestions={suggestions}
+                     onSuggestionSelected={this.onSuggestionSelected}
+                     onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+                     getSuggestionValue={this.getSuggestionValue}
+                     renderSuggestion={this.renderSuggestion}
+                     inputProps={inputProps} />
+      </div>
+    );
+  }
+
+  onChange(event, { newValue }) {
+    this.setState({
+      value: newValue
+    });
+  }
+
+  onSuggestionsUpdateRequested({ value }) {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  }
+
+  getLocations(term) {
+    return [
+      {
+        description: 'New York, NY, USA'
+      },
+      {
+        description: 'Boston, MA, USA'
+      },
+      {
+        description: 'Oakland, CA, USA'
+      },
+      {
+        description: 'Kiev, Ukraine'
+      }
+    ]
+  }
+
+  getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : this.getLocations(inputValue);
+  }
+
+  getSuggestionValue(suggestion) { // when suggestion selected, this function tells what should be the value of the input
+    return suggestion.description;
+  }
+
+  renderSuggestion(suggestion) {
+    return (
+      <span>{suggestion.description}</span>
+    );
+  }
+
+  onSuggestionSelected(event, { suggestion, suggestionValue, sectionIndex, method }) {
+    this.props.onLocationSelected(suggestion);
+  }
+}
+
+LocationSearchBar.propTypes = {
+  onLocationSelected: React.PropTypes.func.isRequired,
+}
+
+export default LocationSearchBar;
