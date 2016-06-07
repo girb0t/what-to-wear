dac47118dbe3eefa6f4569e17c33434a2cadd75e
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-07 13:47:02 -0400

    Make /one-day-forecast the default route

--- app/src/routes.js
+++ app/src/routes.js
@@ -7,6 +7,7 @@ import OneDayForecast from './containers/one_day_forecast';
 
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={OneDayForecast} />
     <Route path="location" component={LocationForm} />
     <Route path="one-day-forecast" component={OneDayForecast} />
   </Route>
